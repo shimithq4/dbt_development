404e6c45f8efd56ceab78798f575f1130e70d5ed
shimith.mathummal@q4inc.com <shimith.mathummal@q4inc.com>
2022-03-08 19:33:23 +0000

    employeedepartment_test

--- models/employee.sql
+++ models/employee.sql
@@ -1,6 +1,10 @@
-select
-        id as employee_id,
-        name as employee_name,
-        Deptno as department_id
+with emp as
+(select 1 as id,'Shimith' as name, 10 as deptno
+union
+select 2 as id,'Anjusha' as name, 10 as deptno
+union
+select 3 as id,'Tanvi' as name, 20 as deptno
+union
+select 4 as id,'Milan' as name, 30 as deptno)
 
-    from atomic.employee
+select * from emp
